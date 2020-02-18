7b063fa3995f27d2863eed3c851f6d59bb8ef049
Poul Henriksen <pohe@easj.dk>
2020-02-18 14:43:10 +0100

    Add project files.

---
+++ MyStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack2
+{
+    public class MyStack<T>
+    {
+        private T[] stack;
+        public int Size { get; }
+        private int top = 0;
+
+        public MyStack(int size)
+        {
+            stack = new T[size];
+            this.Size = size;
+        }
+        public void Push(T element)
+        {
+            if (top == Size) throw new MyStackIsFullException("The stack is full!");
+            stack[top] = element;
+            top++;
+        }
+
+        public T Pop()
+        {
+            if (top == 0) throw new MyStackIsEmptyException("The stack is empty!");
+            return stack[--top];
+        }
+
+        public T Peek()
+        {
+            if (top == 0) throw new MyStackIsEmptyException("The stack is empty!");
+            return stack[top];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>(6);
+
+
+            try
+            {
+                MyStack<int> aStack = new MyStack<int>(4);
+
+                int n = aStack.Pop();
+                //myStack.Push(0);
+                //myStack.Pop();
+                ////Console.WriteLine(myStack.Pop());
+                //myStack.Push(1);
+                //myStack.Push(2);
+                //myStack.Push(3);
+                //myStack.Push(4);
+                //myStack.Push(5);
+                //myStack.Push(6);
+            }
+            catch (MyStackIsEmptyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MyStackIsFullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
